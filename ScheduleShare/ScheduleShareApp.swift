adf5f936541a57c6854486e05c459ae204c1e5a0
Xavierhuang <hhuangweijia@gmail.com>
2025-08-04 22:50:01 -0400

    Initial Commit

---
+++ ScheduleShare/ScheduleShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScheduleShareApp.swift
+//  ScheduleShare
+//
+//  Created by Weijia Huang on 8/4/25.
+//
+
+import SwiftUI
+
+@main
+struct ScheduleShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
